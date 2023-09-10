f11c71a514d54d7a64ad3ceee363b143cb1c0ced
Cera Drake <cera.drake1@gmail.com>
2023-09-10 16:54:53 -0500

    creating sets

--- user_tuples_etc.py
+++ user_tuples_etc.py
@@ -13,3 +13,11 @@ biggest_months = ("February", "May", "June", "September")
 
 logger.info(f"Wholesale locations = {wholesale_locations}")
 logger.info(f"Busies months in the floral industry are: {biggest_months}")
+
+#Sets
+stem_pricing = {2, 3, 4, 7, 9, 10}
+arrangement_pricing = {12, 30, 40, 50, 60}
+
+logger.info(f"The retail stem prices are {stem_pricing}")
+logger.info(f"The arrangement prices are {arrangement_pricing}")
+
