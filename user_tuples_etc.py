1d734b071b7ae795b2bbed17147bb655d16ef359
Cera Drake <cera.drake1@gmail.com>
2023-09-10 16:51:29 -0500

    Creating tuples

--- user_tuples_etc.py
+++ user_tuples_etc.py
@@ -1,4 +1,15 @@
 """
-Modify this docstring.
+Cera Drake 
+Tuples
+9/10/23
 
 """
+from util_datafun_logger import setup_logger
+logger, logname = setup_logger(__file__)
+
+#Create my tuples 
+wholesale_locations = ("Florida", "Ecuador", "Canada", "California", "Bolivia")
+biggest_months = ("February", "May", "June", "September")
+
+logger.info(f"Wholesale locations = {wholesale_locations}")
+logger.info(f"Busies months in the floral industry are: {biggest_months}")
